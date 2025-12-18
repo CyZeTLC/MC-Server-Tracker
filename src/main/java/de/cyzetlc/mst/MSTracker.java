d30548bdc93615ff30b7b8ba7141696230b040af
CyZeTLC <tom@cyzetlc.de>
2025-12-18 18:57:42 +0100

    Initial commit

---
+++ src/main/java/de/cyzetlc/mst/MSTracker.java
@@ -0,0 +1,44 @@
+package de.cyzetlc.mst;
+
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
+public class MSTracker {
+    private static final String API_URL = "https://api.mcsrvstat.us/2/";
+
+    public static void main(String[] args) {
+        if (args.length != 2 || !args[0].equals("-ip")) {
+            System.err.println("Use mst -ip <SERVER_IP>");
+            return;
+        }
+
+        ScheduledExecutorService scheduler = Executors.newSingleThreadScheduledExecutor();
+
+        scheduler.scheduleAtFixedRate(() -> {
+            try {
+                trackServer(args[1]);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }, 0, 5, TimeUnit.MINUTES);
+    }
+
+    public static void trackServer(String ip) throws Exception {
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(API_URL + ip))
+                .build();
+
+        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+
+        String json = response.body();
+
+        System.out.println("Daten empfangen am " + java.time.LocalDateTime.now());
+        System.out.println(json);
+    }
+}
